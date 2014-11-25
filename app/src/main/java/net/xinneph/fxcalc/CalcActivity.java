4091a3523885f00613ea25ef398bc4e7d3661034
xinneph <xinneph@gmail.com>
2014-11-25 23:08:49 +0100

    add stooq functionality

--- app/src/main/java/net/xinneph/fxcalc/CalcActivity.java
+++ app/src/main/java/net/xinneph/fxcalc/CalcActivity.java
@@ -3,7 +3,9 @@ package net.xinneph.fxcalc;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,17 +15,28 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class CalcActivity extends Activity {
+
+    private final static String TAG = CalcActivity.class.getSimpleName();
+    private FragmentPercent fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
         if (savedInstanceState == null) {
+            fragment = new FragmentPercent();
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new FragmentPercent())
+                    .add(R.id.container, fragment)
                     .commit();
         }
     }
@@ -45,7 +58,48 @@ public class CalcActivity extends Activity {
         if (id == R.id.action_settings) {
             return true;
         }
+        else if (id == R.id.action_stooq) {
+            new StooqTask().execute(fragment.getMarket());
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    private URL getUrl(String market) {
+        try {
+            String str = "http://stooq.pl/q/l/?s=" + market + "&f=sd2t2c&e=txt";
+            return new URL(str);
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "", e);
+            return null;
+        }
+    }
+
+    private class StooqTask extends AsyncTask<String,Void,String> {
+        @Override
+        protected String doInBackground(String... input) {
+            URL url = getUrl(input[0]);
+            try {
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(3000);
+                conn.setConnectTimeout(4000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.connect();
+                InputStream stream = conn.getInputStream();
+                byte[] buffer = new byte[128];
+                stream.read(buffer);
+                String content = new String(buffer);
+                return content;
+            } catch (IOException e) {
+                Log.e(TAG, "Error when connecting for URL " + url, e);
+                return null;
+            }
+        };
+
+        @Override
+        protected void onPostExecute(String content) {
+            Toast.makeText(CalcActivity.this, content, Toast.LENGTH_LONG).show();
+        }
     }
 
     /**
@@ -120,6 +174,10 @@ public class CalcActivity extends Activity {
                     android.R.layout.simple_spinner_dropdown_item);
             marketsSpinner.setAdapter(adapter);
             return rootView;
+        }
+
+        public String getMarket() {
+            return (String) marketsSpinner.getSelectedItem();
         }
     }
 }
